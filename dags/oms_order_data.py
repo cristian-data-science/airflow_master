65b83c0759ffc3475ed09a843695ca00ebb15196
eiurrutia <eiurrutia@uc.cl>
2024-04-10 18:39:26 -0400

    feat: oms orders data dag

---
+++ dags/oms_order_data.py
@@ -0,0 +1,223 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from dotenv import load_dotenv
+from dags.config.oms_order_data_config import default_args
+from dags.utils.utils import write_data_to_snowflake
+import os
+import requests
+from requests.exceptions import HTTPError, ChunkedEncodingError
+import time
+import pandas as pd
+
+# Load environment variables from .env file
+load_dotenv()
+OMS_API_CLIENT_ID = os.getenv('OMS_API_CLIENT_ID')
+OMS_API_CLIENT_SECRET = os.getenv('OMS_API_CLIENT_SECRET')
+OMS_API_URL = os.getenv('OMS_API_URL')
+OMS_API_INSTANCE = os.getenv('OMS_API_INSTANCE')
+SNOWFLAKE_CONN_ID = os.getenv('SNOWFLAKE_CONN_ID')
+OMS_TOKEN_URL = f'{OMS_API_URL}authentication/oauth2/token'
+OMS_ORDER_URL = f'{OMS_API_URL}{OMS_API_INSTANCE}/powerbi/order'
+
+# Dag definition
+dag = DAG(
+    'oms_order_data',
+    default_args=default_args,
+    description='DAG to extract order data from OMS '
+    'and write in Snowflake',
+    schedule_interval='*/30 * * * *',
+)
+
+
+class OMSDataFetcher:
+
+    def __init__(self):
+        print('Ejecutó init')
+        self.auth_token = None
+        self.get_auth_token()
+
+    def get_auth_token(self):
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': OMS_API_CLIENT_ID,
+            'client_secret': OMS_API_CLIENT_SECRET
+        }
+        response = requests.post(OMS_TOKEN_URL, data=data)
+        response.raise_for_status()
+        self.auth_token = response.json()['access_token']
+
+    def fetch_oms_orders(self, batch_limit=20, total_limit=50,
+                         db_write_batch_size=100, max_retries=5):
+        headers = {'Authorization': f'Bearer {self.auth_token}'}
+        orders = []
+        offset = 0
+        requests_count = 0
+        total_fetched = 0
+        retries = 0
+
+        while True:
+            if not self.auth_token:
+                self.get_auth_token()
+                headers['Authorization'] = f'Bearer {self.auth_token}'
+
+            try:
+                params = {'limit': batch_limit, 'offset': offset}
+                print(f'[OMS] Getting {batch_limit} orders - offset: {offset}')
+                print(f'[OMS] Request count: {requests_count}')
+                response = requests.get(
+                    OMS_ORDER_URL, headers=headers, params=params
+                )
+                requests_count += 1
+
+                response.raise_for_status()
+                batch = response.json().get('data', [])
+                orders.extend(batch)
+                total_fetched += len(batch)
+
+                if len(orders) >= db_write_batch_size:
+                    print(f'To process {len(orders)} orders')
+                    self.process_orders(orders)
+                    orders = []
+
+                if not batch or (total_limit and total_fetched >= total_limit):
+                    break
+
+                offset += batch_limit
+                time.sleep(1)
+
+            except HTTPError as e:
+                if e.response.status_code == 401:  # Token expired
+                    print('Token expired. Obtaining new token...')
+                    self.get_auth_token()
+                    headers['Authorization'] = f'Bearer {self.auth_token}'
+                    continue
+                retries += 1
+                if max_retries > 0:
+                    print(f'Retrying... attempt {retries}')
+                    time.sleep(2 ** retries)  # Exponential backoff
+                else:
+                    print('Max retries exceeded. Failing the task.')
+                    raise e
+            except (ConnectionError, ChunkedEncodingError) as e:
+                print(f'Connection error encountered: {str(e)}')
+                retries += 1
+                if retries < max_retries:
+                    print(f'Retrying... attempt {retries}')
+                    time.sleep(2 ** retries)  # Exponential backoff
+                else:
+                    print('Max retries exceeded. Failing the task.')
+                    raise e
+            except Exception as e:
+                print(f'Unhandled exception: {str(e)}')
+                raise e
+
+        if orders:
+            print(f'Processing the last batch of {len(orders)} orders')
+            self.process_orders(orders)
+        return orders
+
+    def process_orders(self, orders_list):
+        orders_dataframe = \
+            self.orders_to_dataframe(orders_list)
+
+        print('[AIRFLOW] Orders Dataframe: ')
+        print(orders_dataframe.head().to_string())
+
+        write_data_to_snowflake(
+            orders_dataframe,
+            'OMS_SUBORDERSLINE',
+            default_args['snowflake_oms_suborder_line_data_table_columns'],
+            'LINE_ID',
+            'TEMP_OMS_SUBORDERSLINE',
+            SNOWFLAKE_CONN_ID
+        )
+
+    def orders_to_dataframe(self, orders):
+        orders_data = []
+        for order in orders:
+            transfer_codes = {}
+            for trans in order.get('transfer_warehouse_ids', []):
+                sku = trans['product_id']['sku']
+                warehouse_name = trans['warehouse_name']
+                if sku in transfer_codes:
+                    transfer_codes[sku].add(warehouse_name)
+                else:
+                    transfer_codes[sku] = {warehouse_name}
+            for line in order.get('order_line', []):
+                transfer_warehouse = ', '.join(
+                    transfer_codes.get(line['product_id']['default_code'], [])
+                )
+                orders_data.append({
+                    'LINE_ID': line['id'],
+                    'SUBORDER_ID': order['id'],
+                    'ORDER_ID': order['ecommerce_sale_id']['id'],
+                    'AMOUNT_DISCOUNT': order['amount_discount'],
+                    'AMOUNT_TAX': order['amount_tax'],
+                    'AMOUNT_TOTAL': order['amount_total'],
+                    'AMOUNT_UNTAXED': order['amount_untaxed'],
+                    'STATE_OPTION_NAME':
+                        order['current_state_id']['state_option_id']['name'],
+                    'DATE_ORDER': order['date_order'],
+                    'DELIVERY_CLIENT_DATE': order['delivery_client_date'],
+                    'DELIVERY_METHOD_NAME':
+                        order['delivery_method_id']['name'],
+                    'ECOMMERCE_DATE_ORDER': order['ecommerce_date_order'],
+                    'ECOMMERCE_NAME': order['ecommerce_name'],
+                    'ECOMMERCE_NAME_CHILD': order['ecommerce_name_child'],
+                    'CITY_NAME':
+                        order['ecommerce_sale_id'].get(
+                            'shipment_address_id', {}
+                            ).get('city_id', {}).get('name', ''),
+                    'STREET':
+                        order['ecommerce_sale_id'].get(
+                            'shipment_address_id', {}).get('street', ''),
+                    'PHONE': order['ecommerce_sale_id'].get(
+                        'shipment_address_id', {}).get('phone', ''),
+                    'STATE_NAME': order['ecommerce_sale_id'].get(
+                        'shipment_address_id', {}).get(
+                            'state_id', {}).get('name', ''),
+                    'DISCOUNT': line['discount'],
+                    'PRICE_REDUCE': line['price_reduce'],
+                    'PRICE_TOTAL': line['price_total'],
+                    'PRICE_UNIT': line['price_unit'],
+                    'PRODUCT_UOM_QTY': line['product_uom_qty'],
+                    'DEFAULT_CODE': line['product_id']['default_code'],
+                    'PRODUCT_NAME': line['product_id']['name'],
+                    'EMAIL':
+                        order.get('partner_id', {}).get('email', ''),
+                    'PARTNER_NAME':
+                        order.get('partner_id', {}).get('name', ''),
+                    'PARTNER_PHONE':
+                        order.get('partner_id', {}).get('phone', ''),
+                    'PARTNER_STREET':
+                        order.get('partner_id', {}).get('street', ''),
+                    'PARTNER_VAT':
+                        order.get('partner_id', {}).get('vat', ''),
+                    'PAYMENT_METHOD_NAME':
+                        order.get('payment_method_id', {}).get('name', ''),
+                    'WAREHOUSE': order['warehouse_id']['name'],
+                    'TRANSFER_WAREHOUSE': transfer_warehouse
+                })
+        df = pd.DataFrame(orders_data)
+        df['PARTNER_VAT'] = df['PARTNER_VAT'].astype(
+            str).replace('nan', '').replace('False', '')
+        df = df.drop_duplicates(subset=['LINE_ID'], keep='first')
+        return df
+
+
+def run_get_oms_orders(**context):
+    execution_date = context['execution_date']
+    print(f'Execution Date: {execution_date}')
+    fetcher = OMSDataFetcher()
+    fetcher.fetch_oms_orders(
+        batch_limit=20, total_limit=200,
+        db_write_batch_size=200, max_retries=5
+    )
+
+
+# Task definitions
+task_1 = PythonOperator(
+    task_id='get_oms_orders',
+    python_callable=run_get_oms_orders,
+    dag=dag,
+)
