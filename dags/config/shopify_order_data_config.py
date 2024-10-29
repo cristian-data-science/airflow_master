7ed972b9a28302a2ddfc0abc48fefc6021215dbb
jofigl <josefa.gonzalezl@uc.cl>
2024-10-29 16:08:31 -0300

    arreglo

--- dags/config/shopify_order_data_config.py
+++ dags/config/shopify_order_data_config.py
@@ -7,7 +7,7 @@ default_args = {
     'start_date': datetime(2024, 4, 10, 5, 0),
     'email': ['enrique.urrutia@patagonia.com'],
     'phone': 0,
-    'email_on_failure': False,
+    'email_on_failure': True,
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=60),
