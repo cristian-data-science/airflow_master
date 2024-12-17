2ffef6305bccd6d8561227b62c90c0a93e5ca88f
jofigl <josefa.gonzalezl@uc.cl>
2024-12-17 10:40:33 -0300

    load dollar value config

--- dags/config/exchange_rates_config.py
+++ dags/config/exchange_rates_config.py
@@ -3,11 +3,11 @@ from datetime import timedelta, datetime
 # Define DAG and task in Airflow
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 11, 27, 5, 0),
+    'depends_on_past': True,
+    'start_date': datetime(2024, 12, 16, 5, 0),
     'email': ['enrique.urrutia@patagonia.com'],
     'phone': 0,
-    'email_on_failure': True,
+    'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=60),
