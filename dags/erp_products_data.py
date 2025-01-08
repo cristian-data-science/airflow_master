f0a956b9fcc09953a70056fdc94f93736e0916c0
eiurrutia <eiurrutia@uc.cl>
2025-01-08 13:55:34 -0300

    hotfix: process erp product data in batch

--- dags/erp_products_data.py
+++ dags/erp_products_data.py
@@ -28,22 +28,28 @@ dag = DAG(
 )
 
 
-def get_products_data(cursor, table_name, columns):
+def get_products_data_batch(cursor, table_name, columns, offset, batch_size):
     '''
-    Retrieves product data from the ERP.
+    Retrieves a batch of product data from the ERP.
 
     Parameters:
     - cursor: Cursor for SQL queries execution.
     - table_name (str): Table name for data extraction.
     - columns (str): Columns to select in SQL query.
+    - offset (int): Offset for batch processing.
+    - batch_size (int): Number of records to fetch per batch.
 
     Returns:
-    - pandas.DataFrame: DataFrame containing the product data.
+    - pandas.DataFrame: DataFrame containing the batch data.
     '''
-    query = f'SELECT {columns} FROM {table_name}'
-    print('[BYOD] Executing query')
+    query = f'''
+        SELECT {columns}
+        FROM {table_name}
+        ORDER BY 1  -- Ensure consistent ordering
+        OFFSET {offset} ROWS FETCH NEXT {batch_size} ROWS ONLY
+    '''
+    print(f'[BYOD] Executing query for batch starting at offset {offset}')
     cursor.execute(query)
-    print('[BYOD] Query finished')
     result = cursor.fetchall()
     return pd.DataFrame(result) if result else None
 
@@ -80,10 +86,8 @@ def process_data(df):
 
 def get_products_data_and_load():
     '''
-    Extracts product data from the ERP, processes it, and
-    loads it into Snowflake.
-
-    Fetches all data at once and writes it in one operation.
+    Extracts product data from the ERP in batches, processes it,
+    and loads it into Snowflake.
     '''
     print('[Start execution] Get ERP product data')
 
@@ -91,9 +95,6 @@ def get_products_data_and_load():
     hook = SnowflakeHook(snowflake_conn_id=SNOWFLAKE_CONN_ID)
     conn_snowflake = hook.get_conn()
     cursor_snowflake = conn_snowflake.cursor()
-
-    # Truncate table before loading data
-    truncate_table('ERP_PRODUCTS', SNOWFLAKE_CONN_ID)
 
     # Connect to ERP database
     conn = pymssql.connect(
@@ -105,23 +106,57 @@ def get_products_data_and_load():
         for column in default_args['snowflake_erp_products_table_columns']
     ])
     cursor = conn.cursor(as_dict=True)
-    df = get_products_data(cursor, table_name, columns)
-    if df is not None and not df.empty:
-        processed_df = process_data(df)
-        print('[SNOWFLAKE] Write data')
 
-        # Write data to Snowflake
-        success, nchunks, nrows, _ = write_pandas(
-            conn_snowflake, processed_df, 'ERP_PRODUCTS'
-        )
-        if not success:
-            raise Exception('Failed to write data to Snowflake')
-        print(f'Successfully wrote {nrows} rows to ERP_PRODUCTS in Snowflake.')
-    else:
-        print('[BYOD] Query result empty!')
-    conn.close()
-    cursor_snowflake.close()
-    conn_snowflake.close()
+    # Batch processing variables
+    batch_size = 20000
+    offset = 0
+    batch_number = 1
+
+    # Create a temporary table in Snowflake
+    print('[SNOWFLAKE] Creating temporary table')
+    cursor_snowflake.execute(
+        '''CREATE TEMPORARY TABLE ERP_PRODUCTS_TEMP AS
+        SELECT * FROM ERP_PRODUCTS WHERE 1=0
+    ''')  # Structure only, no data
+
+    try:
+        while True:
+            # Fetch batch from BYOD
+            df = get_products_data_batch(
+                cursor, table_name, columns, offset, batch_size)
+            if df is None or df.empty:
+                break
+
+            # Process batch
+            processed_df = process_data(df)
+
+            # Write batch to Snowflake temporary table
+            print(f'[SNOWFLAKE] Writing batch {batch_number} to'
+                  ' temporary table')
+            success, nchunks, nrows, _ = write_pandas(
+                conn_snowflake, processed_df, 'ERP_PRODUCTS_TEMP'
+            )
+            if not success:
+                raise Exception(f'Failed to write batch {batch_number} '
+                                'to Snowflake')
+
+            print(f'Successfully wrote {nrows} rows for batch {batch_number}')
+            batch_number += 1
+            offset += batch_size
+
+        # If all batches are successfully written, move data to final table
+        print('[SNOWFLAKE] Moving data from temporary table to final table')
+        truncate_table('ERP_PRODUCTS', SNOWFLAKE_CONN_ID)
+        cursor_snowflake.execute(
+            'INSERT INTO ERP_PRODUCTS SELECT * FROM ERP_PRODUCTS_TEMP')
+
+        print('Data successfully loaded into ERP_PRODUCTS in Snowflake.')
+
+    finally:
+        # Close connections
+        conn.close()
+        cursor_snowflake.close()
+        conn_snowflake.close()
 
 
 def run_get_products_data():
