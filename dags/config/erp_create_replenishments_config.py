4af6da7efed6fbffb83b04f95a19a78cefc93d12
eiurrutia <eiurrutia@uc.cl>
2025-05-11 10:51:20 -0400

    faet: erp create replenishment dag

---
+++ dags/config/erp_create_replenishments_config.py
@@ -0,0 +1,29 @@
+from datetime import timedelta
+from airflow.utils.dates import days_ago
+
+# Default variables
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': days_ago(1),
+    'email': ['enrique.urrutia@patagonia.com'],
+    'snowflake_conn_id': 'patagonia_snowflake_connection',
+}
+
+# DAG configuration
+dag_config = {
+    'dag_id': 'erp_create_replenishments',
+    'description': 'Create ERP replenishments from Snowflake data',
+    'schedule_interval': None,  # Only triggered externally
+    'catchup': False,
+    'tags': ['erp', 'replenishment'],
+    # List of email addresses to notify
+    'notification_emails': [
+        'enrique.urrutia@patagonia.com',
+        'eiurrutia@uc.cl'
+    ]
+}
